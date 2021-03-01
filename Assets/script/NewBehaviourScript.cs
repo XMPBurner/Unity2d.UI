1c2e2cce2ff3f6d382941cd0972a63b648606b62
unknown <emilsporikis2@gmail.com>
2021-03-01 16:07:01 +0200

    1.lekcija

---
+++ Assets/script/NewBehaviourScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    string teksts;
+    public GameObject IevadesLauks;
+    public GameObject Textlogs;
+
+    public void uzglabāttekstu(){
+        teksts = IevadesLauks.GetComponent<Text>().text;
+        Textlogs.GetComponent<Text>().text = "Sveiks "+teksts.ToUpper() + "!";
+    }
+}
--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # Unity2d.UI
 pirmais unity mēģinājums
 #### Veicamo darbu saraksts
-- [ ] projekta versionēšana
-- [ ] ievietot teksta logu
+- [x] projekta versionēšana
+- [x] ievietot teksta logu
 - [ ] ievietot ček box
 - [ ] Nodrošināt radio pogas darbību
 - [ ] Ievietos izkrītošana sarakstu
 - [ ] pievienot adio failus
-- [ ] 
