4465c598b41f989d244263f08bf1851f5b1a5074
unknown <emilsporikis2@gmail.com>
2021-03-08 15:10:21 +0200

    progress?

---
+++ Assets/script/NewBehaviourScript1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    public GameObject bins;
+    public GameObject lacitis;
+    public GameObject tante;
+    public GameObject masina;
+
+    public void binaAttelosana(bool vertiba)
+    {
+        bins.SetActive(vertiba);    
+    }
+        public void binaAttelosana(bool vertiba)
+    {
+        lacitis.SetActive(vertiba);    
+    }
+        public void binaAttelosana(bool vertiba)
+    {
+        tante.SetActive(vertiba);    
+    }
+        public void binaAttelosana(bool vertiba)
+    {
+        masina.SetActive(vertiba);    
+    }
+}
